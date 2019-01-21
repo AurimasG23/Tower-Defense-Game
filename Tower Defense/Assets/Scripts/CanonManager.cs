611ac37b7dcdfe5a70e374b07c3a3125a3bd7d08
AurimasG23 <aurim.gecas@gmail.com>
2019-01-21 22:54:07 +0200

    .

--- Tower Defense/Assets/Scripts/CanonManager.cs
+++ Tower Defense/Assets/Scripts/CanonManager.cs
@@ -13,6 +13,8 @@ public class CanonManager : MonoBehaviour
     public GameObject laikikliai;
     public GameObject vamzdis;
     public GameObject vamzdzio_laikiklis;
+    public GameObject varztai_dideli;
+    public GameObject varztai_mazi;
 
 
 
@@ -36,16 +38,20 @@ public class CanonManager : MonoBehaviour
         laikikliai.transform.position = new Vector3(laikikliai.transform.position.x + CameraDirection.x, laikikliai.transform.position.y + CameraDirection.y, laikikliai.transform.position.z + CameraDirection.z);
         vamzdis.transform.position = new Vector3(vamzdis.transform.position.x + CameraDirection.x, vamzdis.transform.position.y + CameraDirection.y, vamzdis.transform.position.z + CameraDirection.z);
         vamzdzio_laikiklis.transform.position = new Vector3(vamzdzio_laikiklis.transform.position.x + CameraDirection.x, vamzdzio_laikiklis.transform.position.y + CameraDirection.y, vamzdzio_laikiklis.transform.position.z + CameraDirection.z);
+        varztai_dideli.transform.position = new Vector3(varztai_dideli.transform.position.x + CameraDirection.x, varztai_dideli.transform.position.y + CameraDirection.y, varztai_dideli.transform.position.z + CameraDirection.z);
+        varztai_mazi.transform.position = new Vector3(varztai_mazi.transform.position.x + CameraDirection.x, varztai_mazi.transform.position.y + CameraDirection.y, varztai_mazi.transform.position.z + CameraDirection.z);
     }
 
     public void PutBack()
     {
         zole.transform.localPosition= new Vector3(0, 0, 0);
-        remas.transform.localPosition = new Vector3(0, 1.18f, 0);
-        platforma.transform.localPosition = new Vector3(0, 1.18f, 0);
-        begiai.transform.localPosition = new Vector3(0, 1.18f, 0);
-        laikikliai.transform.localPosition = new Vector3(0, 1.18f, 0);
-        vamzdis.transform.localPosition = new Vector3(0, 1.18f, 0);
-        vamzdzio_laikiklis.transform.localPosition = new Vector3(0, 1.18f, 0);
+        remas.transform.localPosition = new Vector3(0, 0, 0);
+        platforma.transform.localPosition = new Vector3(0, 0, 0);
+        begiai.transform.localPosition = new Vector3(0, 0, 0);
+        laikikliai.transform.localPosition = new Vector3(0, 0, 0);
+        vamzdis.transform.localPosition = new Vector3(0, 0, 0);
+        vamzdzio_laikiklis.transform.localPosition = new Vector3(0, 0, 0);
+        varztai_dideli.transform.localPosition = new Vector3(0, 0, 0);
+        varztai_mazi.transform.localPosition = new Vector3(0, 0, 0);
     }
 }
