d13612282a602737d6c4fd6f826db3c2ff63c5af
AurimasG23 <aurim.gecas@gmail.com>
2019-03-04 21:42:54 +0200

    .

---
+++ Tower Defense/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    float speed = 10f;
+
+    private Transform target;
+    private int wayPointIndex = 0;
+
+	// Use this for initialization
+	void Start ()
+    {
+        target = WayPoints.points[0];
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Vector3 direction = target.position - transform.position;
+        transform.Translate(direction.normalized * speed * Time.deltaTime, Space.World);
+
+        if(Vector3.Distance(transform.position, target.position) <= 0.1f)
+        {
+            GetNextWayPoint();
+        }
+	}
+
+    void GetNextWayPoint()
+    {
+        if(wayPointIndex >= WayPoints.points.Length - 1)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        wayPointIndex++;
+        target = WayPoints.points[wayPointIndex];
+    }
+}
