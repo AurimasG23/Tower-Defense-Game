20d63612805e298d620cef43d9c21b27b68ba8c3
AurimasG23 <aurim.gecas@gmail.com>
2019-03-05 23:28:24 +0200

    .

---
+++ Tower Defense/Assets/Scripts/GamePlay/EnemySpawner.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public Transform enemyPrefab;
+
+    float timeBetweenWaves = 5f;
+    float countDown = 2f;
+    int waveNumber = 0;
+
+    public Transform spawnPoint;
+
+
+    private void Update()
+    {
+        if(countDown <= 0f)
+        {
+            StartCoroutine(SpawnWave());
+            countDown = timeBetweenWaves;
+        }
+
+        countDown -= Time.deltaTime;
+    }
+
+    IEnumerator SpawnWave()
+    {
+        waveNumber++;
+
+        for (int i = 0; i < waveNumber; i++)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(0.3f);
+        }        
+    }
+
+    void SpawnEnemy()
+    {
+        Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
+}
