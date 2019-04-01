de0efcf455c0dd3219a5fdf0cc09690b32716aa9
AurimasG23 <aurim.gecas@gmail.com>
2019-04-01 18:50:30 +0300

    .

---
+++ Tower Defense/Assets/Scripts/Laborams/DoorsForceManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorsForceManager : MonoBehaviour
+{
+    public GameObject doorsBlue;
+    public GameObject doorsRed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OpenDoors()
+    {
+        doorsBlue.transform.GetComponent<Rigidbody>().AddForce(new Vector3(doorsBlue.transform.rotation.x, doorsBlue.transform.rotation.y * 1000, doorsBlue.transform.rotation.z), ForceMode.Acceleration);
+        doorsBlue.transform.GetComponent<Rigidbody>().useGravity = true;
+        doorsRed.transform.GetComponent<Rigidbody>().AddForce(new Vector3(doorsRed.transform.rotation.x, doorsRed.transform.rotation.y * 1000, doorsRed.transform.rotation.z), ForceMode.Acceleration);
+        doorsRed.transform.GetComponent<Rigidbody>().useGravity = true;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "character")
+        {
+            OpenDoors();
+        }
+    }
+}
