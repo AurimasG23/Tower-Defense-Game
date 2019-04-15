f61603ee6d2fe9ac7afa71ee407290a43db8b2b1
AurimasG23 <aurim.gecas@gmail.com>
2019-04-15 15:09:33 +0300

    .

---
+++ Tower Defense/Assets/Laborams/gynimas.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gynimas : MonoBehaviour
+{
+    public PhysicMaterial slidiMaterial;
+
+	// Use this for initialization
+	void Start ()
+    {
+        slidiMaterial.dynamicFriction = 0;
+        slidiMaterial.staticFriction = 0;
+        slidiMaterial.bounciness = 0;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(Input.GetKey(KeyCode.P))
+        {
+            IsjungtiSliduma();
+        }
+        if (Input.GetKey(KeyCode.L))
+        {
+            IjungtiSliduma();
+        }
+    }
+
+    private void IsjungtiSliduma()
+    {
+        slidiMaterial.dynamicFriction = 1000000000000000000;
+        slidiMaterial.staticFriction = 1000000000000000000;
+    }
+
+    private void IjungtiSliduma()
+    {
+        slidiMaterial.dynamicFriction = 0;
+        slidiMaterial.staticFriction = 0;
+    }
+}
