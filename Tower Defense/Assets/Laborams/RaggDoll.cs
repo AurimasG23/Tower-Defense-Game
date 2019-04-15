3bebf3804f3f371e37796a5cd129f8d3c5745ddd
AurimasG23 <aurim.gecas@gmail.com>
2019-04-15 13:27:50 +0300

    .

---
+++ Tower Defense/Assets/Laborams/RaggDoll.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaggDoll : MonoBehaviour {
+
+
+    public GameObject character;
+    public GameObject characterRagDoll;
+
+    // Use this for initialization
+    void Start ()
+    {
+        addRagdoll();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void addRagdoll()
+    {
+        Instantiate(characterRagDoll, character.transform.position, Quaternion.identity);
+        character.SetActive(false);
+    }
+}
